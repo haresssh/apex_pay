27abdd77349238a36ddc31737997da833de43de0
Haresh Prajapati <hp494343@gmail.com>
2026-03-14 18:01:49 +0530

    chore: dockerise frontend

--- apex-pay-ui/vite.config.js
+++ apex-pay-ui/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This exposes the server to the Docker network
+    port: 5173,
+    watch: {
+      usePolling: true, // Required for Docker hot-reloading on some OS architectures
+    }
+  },
 })
